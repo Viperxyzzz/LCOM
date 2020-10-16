e2c6e27fdae57529df5d02f5dbd532844688349b
Diogo Alexandre Paredes Azevedo Costa e SA! <up201905383@fe.up.pt>
2020-10-16 18:39:40 +0100

    Update i8254.h

--- lab2/i8254.h
+++ lab2/i8254.h
@@ -9,7 +9,9 @@
  * Constants for programming the i8254 Timer. Needs to be completed.
  */
 
+#define DEFAULT_FREQ 60 /**@brief Timer 0's default frequency*/
 #define TIMER_FREQ 1193182 /**< @brief clock frequency for timer in PC and AT */
+#define BIT(n) (0x01<<(n))
 #define TIMER0_IRQ 0 /**< @brief Timer 0 IRQ line */
 
 /* I/O port addresses */
